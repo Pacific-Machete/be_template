ab262cb561f5ad31f0cec24b89f9956112a13e21
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2022-12-02 15:43:54 +0900

    Initial commit

---
+++ src/main/java/com/mari/pm/be_template/BeTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.mari.pm.be_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeTemplateApplication.class, args);
+    }
+
+}
